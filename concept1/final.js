fae33e2f64f3ca8dc7cacc2420ebbbd246410282
lilyhsinhai <lha5017@nyu.edu>
2023-04-27 15:13:50 -0400

    free drag and drop

--- concept1/final.js
+++ concept1/final.js
@@ -4,41 +4,41 @@ const target = document.querySelector('#target')
 
 
 clothes1.forEach((clothesImage) => {
-	clothesImage.addEventListener('dragstart', (e) => {
-		e.dataTransfer.setDragImage(clothesImage, clothesImage.width/2, clothesImage.height/2);
-		e.dataTransfer.setData("src-id", clothesImage.id);
-		e.target.style.opacity = 0.3;
-	});
+	clothesImage.onmousedown = function(event) {
 
-	clothesImage.addEventListener('dragend', (e) => {
-		e.target.style.opacity = 1;
-	})
+  let shiftX = event.clientX - clothesImage.getBoundingClientRect().left;
+  let shiftY = event.clientY - clothesImage.getBoundingClientRect().top;
 
-})
+  clothesImage.style.position = 'absolute';
+  clothesImage.style.zIndex = 1000;
+  document.body.append(clothesImage);
 
+  moveAt(event.pageX, event.pageY);
 
-target.addEventListener('dragover', (e) => {
-	e.preventDefault();
-})
+  // moves the ball at (pageX, pageY) coordinates
+  // taking initial shifts into account
+  function moveAt(pageX, pageY) {
+    clothesImage.style.left = pageX - shiftX + 'px';
+    clothesImage.style.top = pageY - shiftY + 'px';
+  }
 
-target.addEventListener('dragenter', (e) => {
-	e.target.style.opacity = 0.3;
-})
+  function onMouseMove(event) {
+    moveAt(event.pageX, event.pageY);
+  }
 
-target.addEventListener('dragleave', (e) => {
-	e.target.style.opacity = 1;
-})
+  // move the ball on mousemove
+  document.addEventListener('mousemove', onMouseMove);
 
-target.addEventListener('drop', (e) => {
-	e.preventDefault();
-	const srcId = e.dataTransfer.getData("src-id");
-	document.querySelector('#' + srcId).classList.add('worn');
-	e.target.style.opacity = 1;
-})
+  // drop the ball, remove unneeded handlers
+  clothesImage.onmouseup = function() {
+    document.removeEventListener('mousemove', onMouseMove);
+    clothesImage.onmouseup = null;
+  };
 
-target.addEventListener('dblclick', (e) => {
-	e.preventDefault();
-	clothes1.forEach((clothesImage) => {
-		clothesImage.classList.remove('worn');	
-	})
+};
+
+clothesImage.ondragstart = function() {
+  return false;
+};
+
 })
