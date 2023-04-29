777f81c5ce0141140350d3249878b55385ede19d
lilyhsinhai <lha5017@nyu.edu>
2023-04-29 15:17:39 -0400

    reset animation

--- final/final.js
+++ final/final.js
@@ -1,50 +1,53 @@
 
 const clothes1 = document.querySelectorAll('.clothes-image');
-const reset = document.querySelector('.reset');
+const reset = document.querySelector('#reset');
 
 
 clothes1.forEach((clothesImage) => {
 	clothesImage.onmousedown = function(event) {
+    
+    clothesImage.classList.remove("resetting");
 
-  let shiftX = event.clientX - clothesImage.getBoundingClientRect().left;
-  let shiftY = event.clientY - clothesImage.getBoundingClientRect().top;
+    let shiftX = event.clientX - clothesImage.getBoundingClientRect().left;
+    let shiftY = event.clientY - clothesImage.getBoundingClientRect().top;
 
-  // clothesImage.style.position = 'absolute';
-  // clothesImage.style.zIndex = 1000;
-  // document.body.append(clothesImage);
+    // clothesImage.style.position = 'absolute';
+    // clothesImage.style.zIndex = 1000;
+    // document.body.append(clothesImage);
 
-  moveAt(event.pageX, event.pageY);
-
-  // moves the clothes at (pageX, pageY) coordinates
-  // taking initial shifts into account
-  function moveAt(pageX, pageY) {
-    clothesImage.style.left = `${pageX - shiftX}px`;
-    clothesImage.style.top = `${pageY - shiftY}px`;
-  }
-
-  function onMouseMove(event) {
     moveAt(event.pageX, event.pageY);
-  }
 
-  // move the clothes on mousemove
-  document.addEventListener('mousemove', onMouseMove);
+    // moves the clothes at (pageX, pageY) coordinates
+    // taking initial shifts into account
+    function moveAt(pageX, pageY) {
+      clothesImage.style.left = `${pageX - shiftX}px`;
+      clothesImage.style.top = `${pageY - shiftY}px`;
+    }
 
-  // drop the clothes, remove unneeded handlers
-  clothesImage.onmouseup = function() {
-    document.removeEventListener('mousemove', onMouseMove);
-    clothesImage.onmouseup = null;
+    function onMouseMove(event) {
+      moveAt(event.pageX, event.pageY);
+    }
+
+    // move the clothes on mousemove
+    document.addEventListener('mousemove', onMouseMove);
+
+    // drop the clothes, remove unneeded handlers
+    clothesImage.onmouseup = function() {
+      document.removeEventListener('mousemove', onMouseMove);
+      clothesImage.onmouseup = null;
+    };
+
   };
 
-};
-
-clothesImage.ondragstart = function() {
-  return false;
-};
+  clothesImage.ondragstart = function() {
+    return false;
+  };
 
 });
 
 reset.onclick = function(event) {
   clothes1.forEach((clothesImage) => {
+    clothesImage.classList.add("resetting");
     clothesImage.style = "";
   });
 }
